199972dbefbe4bf96c8950b1ecf2d125e263edd2
mawalou14 <awalou213@gmail.com>
2023-11-14 22:39:52 +0100

    connecting to db

---
+++ db.js
@@ -0,0 +1,12 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "students",
+    password: "6292568",
+    port: 5432,
+});
+
+module.exports = pool;
+
