4f76319df7cbf37706ff45391295027850adad1a
mawalou14 <awalou213@gmail.com>
2023-11-14 22:45:59 +0100

    created api/v1/students route

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+
+// Imports
+const studentRoute = require('./src/student/routes');
+
+
 const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello world");
 })
+
+app.use('/api/v1/students', studentRoute);
 
 app.listen(port, () => console.log(`app listening on port ${port}`));   
